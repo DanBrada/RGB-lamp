66578170671c3c42282d589827939f9a20ef5ef8
Daniel Brada <danik.brada@seznam.cz>
2022-11-07 12:48:36 +0100

    init

---
+++ hw/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <BLEServer.h>
+void setup() {
+// write your initialization code here
+
+}
+
+void loop() {
+// write your code here
+}
